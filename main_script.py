63ba9addd7976e6c6c54a991d1b0cb41bc613ed7
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-31 19:45:32 -0500

    created next_board_state function to calculate the next state of the board

--- main_script.py
+++ main_script.py
@@ -37,6 +37,7 @@ def render(board):
     #print bottom of board
     print('- '*board_width)
 
+#tests the render function which prints the board to the screen
 def test_render():
     dead_test = dead_state(width,height)
     render(dead_test)
@@ -44,8 +45,31 @@ def test_render():
     random_test = random_state(width,height)
     render(random_test)
 
+#calculates the next state of the board
+def next_board_state(board):
+    next_board = board #this array will store boolean values and update after population
+
+    #iterate over board and check for conditions
+    width = len(board[0])
+    height = len(board)
+
+    for i in range(height):
+        for j in range(width):
+            # any live cell with 0 or 1 neighbors -->  dead
+            # any live cell with 2 or 3 live neighbors stays alive
+            # any live cell with > 3 live neighbors --> dead
+            # any dead cell with 4 neightbors --> alive
+            
+            neighbors = [None,None,None,None] #in order of left_neighbor, top_neighbor, bottom_neighbor, right_neighbor, not like it actually matters
+            
+            if board[i][j]==0: #cell is dead
+                pass
+            else: #cell is alive
+                pass
+
 def main():
-    test_render()
+    board = random_state(width,height)
+
 
 if __name__ == '__main__':
     main()
